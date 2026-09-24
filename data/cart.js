d28449d9a8b3b53b0b6c35584b12f082ec04500f
Varshini <varu91490@gmail.com>
2026-09-24 20:28:17 +0530

    generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,30 @@
- export const cart=[];
+ export const cart=[{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2
+ },{
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:1
+ }];
+
+ //function to add a product to cart like what functions should be done beofre adding it
+export function addtoCart(product){
+
+    let matchingItem;
+
+    cart.forEach((cartitem) => {
+      if(cartitem.productId===productId){
+        matchingItem=cartitem;
+        return;
+      }
+    });
+
+
+    if(matchingItem){
+      matchingItem.quantity+=1;
+    } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import{cart} from '../data/cart.js';
+import{cart, addtoCart} from '../data/cart.js';
 import{products} from '../data/products.js';
 
 let productsHTML='';
@@ -61,36 +61,27 @@ products.forEach((product)=> {
 document.querySelector('.products-grid')
 .innerHTML=productsHTML;
 
-document.querySelectorAll('.add-to-cart-button')
-.forEach((button) => {
-  button.addEventListener('click', () => {
-    const product =button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if(item.productId===product){
-        matchingItem=item;
-        return;
-      }
-    });
 
 
-    if(matchingItem){
-      matchingItem.quantity+=1;
-    } else {
-    cart.push({
-      productId: product,
-      quantity: 1
-    });
-  }
-
+//function to update the cart result in frontend view
+function updateCartQuantity(){
   let cartQuantity=0;
-  cart.forEach((item) => {
-    cartQuantity+=item.quantity;
+
+  cart.forEach((cartitem) => {
+    cartQuantity+=cartitem.quantity;
   });
 
   document.querySelector('.cart-quantity')
   .innerHTML=cartQuantity;
+}
+
+
+document.querySelectorAll('.add-to-cart-button')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId =button.dataset.productId;
+
+    addtoCart(productId);
+    updateCartQuantity();
   });
 });
