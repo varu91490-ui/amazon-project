23dffc18e636786b99bb7eda70afd1a793d809bd
Varshini <varu91490@gmail.com>
2026-09-25 19:17:23 +0530

    modified and removed some errors

--- data/cart.js
+++ data/cart.js
@@ -1,38 +1,31 @@
- export let cart=JSON.parse(localStorage.getItem('cart'));
- 
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
- if(!cart){
-  cart=[{
-  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
- },{
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:1
- }];
- }
- 
-//to store in locl storage so even after refreshing the chages we made will be stored
- function saveToStorage(){
-  localStorage.setItem('cart',JSON.stringify(cart));
- }
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
 
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
- //function to add a product to cart like what functions should be done beofre adding it
-export function addtoCart(productId){
+export function addToCart(productId) {
+  let matchingItem;
 
-    let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-    cart.forEach((cartitem) => {
-      if(cartitem.productId===productId){
-        matchingItem=cartitem;
-        return;
-      }
-    });
-
-
-    if(matchingItem){
-      matchingItem.quantity+=1;
-    } else {
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
     cart.push({
       productId: productId,
       quantity: 1
@@ -42,14 +35,16 @@ export function addtoCart(productId){
   saveToStorage();
 }
 
-export function removeFromCart(productId){
-  //create a new array with all the items except the one to be removed
-  const newCart=[];
-  cart.forEach((cartitem) => {
-    if(cartitem.productId!==productId){
-      newCart.push(cartitem);
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
     }
   });
-  cart=newCart;
+
+  cart = newCart;
+
   saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,88 +1,81 @@
-import{cart, addtoCart} from '../data/cart.js';
-import{products} from '../data/products.js';
-import{formatCurrency} from './utils/money.js';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-let productsHTML='';
+let productsHTML = '';
 
-products.forEach((product)=> {
-  productsHTML+=`
-  <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
-
-          <div class="product-price">
-            $${formatCurrency(product.priceCents)}
-          </div>
-
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary"
-          data-product-id="${product.id}">
-            Add to Cart
-          </button>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
         </div>
+      </div>
+
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
   `;
-})
+});
 
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelector('.products-grid')
-.innerHTML=productsHTML;
+function updateCartQuantity() {
+  let cartQuantity = 0;
 
-
-
-//function to update the cart result in frontend view
-function updateCartQuantity(){
-  let cartQuantity=0;
-
-  cart.forEach((cartitem) => {
-    cartQuantity+=cartitem.quantity;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
   });
 
-  document.querySelector('.cart-quantity')
-  .innerHTML=cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
-
-document.querySelectorAll('.add-to-cart-button')
-.forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId =button.dataset.productId;
-
-    addtoCart(productId);
-    updateCartQuantity();
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
   });
-});
